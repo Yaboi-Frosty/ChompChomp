3a93acf36a102193d955395d781d9cb104609da3
Calneh <franckecalneh@gmail.com>
2020-03-20 11:56:08 +1100

    Scenes Registration and MainMenu

---
+++ Chomp Chomp/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+//put on main menu canvas. makes buttons interactive.
+{
+
+    public Text playerdisplay;
+
+    public void GotoRegistration()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GotoLogin()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void GotoPlay()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
