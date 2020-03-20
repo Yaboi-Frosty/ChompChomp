7214080fddfc3dd5f9c0fb7d0a4d7fffd99d976a
Calneh <franckecalneh@gmail.com>
2020-03-20 12:55:18 +1100

    fixed scenes

---
+++ Chomp Chomp/Assets/Scripts/UI/RegisterScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RegisterScript : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button SubmitButton; 
+
+   public void VerifyInputs()
+    {
+        SubmitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
